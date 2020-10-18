1480e129cfa5654745e85543f23322a033418539
Jonathan White <support@dmapps.us>
2020-10-18 11:41:21 -0400

    Move global shortcut handling into OSUtils

--- src/gui/osutils/OSUtilsBase.h
+++ src/gui/osutils/OSUtilsBase.h
@@ -50,6 +50,14 @@ public:
      */
     virtual bool isCapslockEnabled() = 0;
 
+    virtual void registerNativeEventFilter() = 0;
+
+    virtual bool registerGlobalShortcut(const QString& name, Qt::Key key, Qt::KeyboardModifiers modifiers) = 0;
+    virtual bool unregisterGlobalShortcut(const QString& name) = 0;
+
+signals:
+    void globalShortcutTriggered(const QString& name);
+
 protected:
     explicit OSUtilsBase(QObject* parent = nullptr);
     virtual ~OSUtilsBase();
